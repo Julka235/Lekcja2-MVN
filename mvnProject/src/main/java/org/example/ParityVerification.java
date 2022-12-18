0ff3601538afce70a0972a69e39c6e29ce29bee7
Julia Czosnek <100991936+Julka235@users.noreply.github.com>
2022-12-18 18:23:33 +0100

    Update ParityVerification.java

--- mvnProject/src/main/java/org/example/ParityVerification.java
+++ mvnProject/src/main/java/org/example/ParityVerification.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class ParityVerification {
-    public static boolean verifyIsEven(int number) {
+    public static boolean IsEven(int number) {
         return (number % 2 == 0);
+    }
+
+    public static boolean IsOdd(int number) {
+        return (number % 2 == 1);
     }
 }
